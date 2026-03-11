1c8965d0c3dd6b25e9c065e20720aa9d9e5aa033
yagnasrikadavakollu0810-eng <yagnasrikadavakollu0810@gmail.com>
2026-03-11 17:29:01 +0530

    Create Check-models.py

---
+++ Check-models.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+from groq import Groq
+
+#Load environment variables
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+#Fetch the list of models
+#The models.list() call returns a SyncCursorPage object
+models = client.models.list()
+
+print("Models available for your key:")
+#Access the 'data' attribute to iterate through the model objects for model in models.data:
+  prin(f" - {model.id}")
